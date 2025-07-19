5951b807364e9443aec2a1a0334da9f3b74f1c25
PedroCaldas13 <pedro1306@academico.ufs.br>
2025-07-19 19:16:41 -0300

    Upando a Classe Produto e as suas subclasses

--- src/loja/model/produto/ProdutoDigital.java
+++ src/loja/model/produto/ProdutoDigital.java
@@ -1,3 +1,23 @@
-public class ProdutoDigital {
-    
+import java.math.BigDecimal;
+
+public class ProdutoDigital extends Produto{
+
+    private String linkDoProduto; // link do produto
+
+    public ProdutoDigital(String codigo, String nome, BigDecimal preco,BigDecimal estoque, String linkDoProduto){
+        super(codigo, nome,preco,estoque);
+        this.linkDoProduto = linkDoProduto;
+    }
+
+    //metodos
+    public String getLinkDoProduto(){
+        return linkDoProduto;
+    }
+
+    public void setLinkDoProduto(String linkDoProduto){
+        if(linkDoProduto==null|| linkDoProduto.trim().isEmpty()){
+            throw new IllegalArgumentException("O link do produto não pode ser nulo ou vazio."); //sinaliza que houve erro
+        }
+        this.linkDoProduto = linkDoProduto;
+    }
 }
