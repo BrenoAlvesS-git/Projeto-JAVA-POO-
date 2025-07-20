4f6a689825094e74db8d831464dbdf06851a19d6
PedroCaldas13 <pedro1306@academico.ufs.br>
2025-07-19 21:23:35 -0300

    upando o itemnota

--- src/loja/model/nota/ItemNota.java
+++ src/loja/model/nota/ItemNota.java
@@ -1,5 +1,51 @@
 package loja.model.nota;
 
 public class ItemNota {
-    
+    private Produto produto;
+
+    private int quantidade;
+
+
+
+    public ItemNota(Produto produto, int quantidade){
+
+        this.produto = produto;
+
+        this.quantidade = quantidade;
+
+    }
+
+
+
+//metodos
+
+//get
+
+
+
+    public Produto getProduto() {
+
+        return produto;
+
+    }
+
+
+
+    public int getQuantidade() {
+
+        return quantidade;
+
+    }
+
+
+
+
+
+//Calculo do valor do item
+
+    public BigDecimal getValorDoItem() {
+
+        return produto.getPreco().multiply(BigDecimal.valueOf(quantidade)); //calculo do valor do item usando o preco base do produto
+
+    }
 }
